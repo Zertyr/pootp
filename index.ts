a3018f0332f3d1ef05b8518593ab8b80b38ee4e4
Damien <Damino95@gmail.com>
2022-04-05 14:10:41 +0200

    add array

--- index.ts
+++ index.ts
@@ -7,8 +7,8 @@ import Animal from './Interfaces/Animal';
 //-----------------------------//
 // Création des instances //
 
-const chat: Chat = new Chat("Halliday", 10, new Date("2018/01/31"));
-const dauphin: Dauphin = new Dauphin("Larbin", 25, new Date("2020/12/31"));
+const chat: Chat = new Chat("Kiba", 10, new Date("2018/01/31"));
+const dauphin: Dauphin = new Dauphin("Flipper", 25, new Date("2020/12/31"));
 
 console.log('-------------------START 1-----------------------');
 //On affiche le chat qui sait nager et marcher
@@ -41,3 +41,19 @@ console.log('|----------------------------------------------------|');
 //On affiche le dauphin sans l'information nager & respirer sous l'eau
 dauphin.display();
 console.log('-------------------END 3-----------------------');
+
+const  animaux: Animal[] = [chat,dauphin];
+const  mammiferes: Mammifere[] = [chat,dauphin];
+
+
+console.log('-------------------START 4-----------------------');
+//On affiche le tableau des animaux
+console.log(animaux);
+//On affiche les informations des animaux
+animaux.forEach(animal => animal.display() );
+console.log('|----------------------------------------------------|');
+//On affiche le tableau des mammifères
+console.log(mammiferes);
+//On affiche les mammifères de manière propre
+mammiferes.forEach(mammifere => console.log(`Name : ${mammifere._nom} | birthday : ${mammifere.dateNaissance}, age : ${mammifere.age} ans`));
+console.log('-------------------END 4-----------------------');
